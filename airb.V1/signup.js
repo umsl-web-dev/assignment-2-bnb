ca5161b5b1a3a0238167b18fa35cbb63c043ce3d
github <jhumphrey33@yahoo.com>
2019-12-09 15:41:50 -0600

    final push

--- airb.V1/signup.js
+++ airb.V1/signup.js
@@ -1,7 +1,29 @@
-emailS = document.getElementById("email").value;
+"use strict";
 
 
-checker = () => 
-{    
-    alert(emailS);
-}
+function passValid()
+{   
+    var firstPass=document.getElementById("pass").value;
+    var name=document.getElementById("eID").value;  
+    var cPass=document.getElementById("cPass").value;  
+     
+    console.log(name)
+
+    if (firstPass.length<6  && cPass.length<6)
+      {  
+        alert("Password must be at least 6 characters long.");  
+        return false;  
+      }      
+            
+    if (firstPass===cPass)
+      {  
+          return true;  
+      }  
+    else
+      {  
+          alert("password must be same!");  
+          return false;  
+      }  
+    
+     
+}  
